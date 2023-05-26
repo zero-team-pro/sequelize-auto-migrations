f2e9c19539a11364d32b0a8ef716375c448a6d2a
Platon Efimov <platon.efimov@gmail.com>
2023-05-26 23:40:51 +0700

    Update to current changes

--- example/models/country.js
+++ example/models/country.js
@@ -1,26 +1,29 @@
 /* eslint-disable key-spacing, no-multi-spaces */
 
 module.exports = function (DB, { INTEGER, STRING, BOOLEAN }) {
-  const Model = DB.define('country',
+  const Model = DB.define(
+    "country",
     {
-      id:       { type: INTEGER, allowNull: false, primaryKey: true, autoIncrement: true },
-      title:    { type: STRING,  allowNull: false },
-      display:  { type: BOOLEAN, allowNull: false, defaultValue: true }
+      id: {
+        type: INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: { type: STRING, allowNull: false },
+      display: { type: BOOLEAN, allowNull: false, defaultValue: true },
     },
     {
       timestamps: false,
       underscored: true,
-      tableName: 'country',
-      indexes: [
-        { fields: [ 'title' ] },
-        { fields: [ 'display' ] }
-      ]
+      tableName: "country",
+      indexes: [{ fields: ["title"] }, { fields: ["display"] }],
     }
-  )
+  );
 
   Model.associate = (models) => {
-    Model.hasMany(models.city, { foreignKey: { allowNull: false } })
-  }
+    Model.hasMany(models.city, { foreignKey: { allowNull: false } });
+  };
 
-  return Model
-}
+  return Model;
+};
--- example/models/purchaseProducts.js
+++ example/models/purchaseProducts.js
@@ -3,19 +3,18 @@
 // @fix: https://github.com/flexxnn/sequelize-auto-migrations/issues/21
 
 module.exports = function (DB, { INTEGER, DECIMAL }) {
-    const purchaseProducts = DB.define('purchaseProducts', {
-        id: {
-          type: INTEGER.UNSIGNED,
-          allowNull: false,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        price: {
-          type: DECIMAL(6, 2),
-          allowNull: false,
-        },
-    });
-    
-    return purchaseProducts;
-  }
-  
+  const purchaseProducts = DB.define("purchaseProducts", {
+    id: {
+      type: INTEGER.UNSIGNED,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    price: {
+      type: DECIMAL(6, 2),
+      allowNull: false,
+    },
+  });
+
+  return purchaseProducts;
+};
